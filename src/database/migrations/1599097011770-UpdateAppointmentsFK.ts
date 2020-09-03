96f70d22f9bf4bbfb8ef4eee2f2d094a485eb021
unknown <bittencourtcrb@gmail.com>
2020-09-02 23:31:17 -0300

    Create User

---
+++ src/database/migrations/1599097011770-UpdateAppointmentsFK.ts
@@ -0,0 +1,27 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class UpdateAppointmentsFK1599097011770
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+  }
+}
